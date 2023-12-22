317bba28ec43cd363a9843fe92cb9fac0a53e26c
Harun Yılmaz <113914227+harnylmzz@users.noreply.github.com>
2023-12-22 07:54:56 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Dashboard from './layouts/Dashboard';
-import 'semantic-ui-css/semantic.min.css'
+import 'semantic-ui-css/semantic.min.css';
 import { Container } from 'semantic-ui-react';
 import Navi from './layouts/Navi';
 
